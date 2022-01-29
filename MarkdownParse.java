a5149518aa12e076762bb2a08f17f145494bf85d
ad656 <allandong656@gmail.com>
2022-01-28 21:23:18 -0800

    added if statements

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -12,11 +12,15 @@ public class MarkdownParse {
         int currentIndex = 0;
         while(currentIndex < markdown.length()) {
             int nextOpenBracket = markdown.indexOf("[", currentIndex);
+            if(nextOpenBracket>=0 ){
             int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
             int openParen = markdown.indexOf("(", nextCloseBracket);
             int closeParen = markdown.indexOf(")", openParen);
             toReturn.add(markdown.substring(openParen + 1, closeParen));
             currentIndex = closeParen + 1;
+        }
+        else
+        currentIndex = markdown.length();
         }
         return toReturn;
     }
--- test-file.md
+++ test-file.md
@@ -2,3 +2,5 @@
 
 [a link!](https://something.com)
 [another link!](some-page.html)
+[3rd link](www.google.com)
+random text
