aba8990e391e8dbc16c9b5f7630a0427d9d33d50
ad656 <allandong656@gmail.com>
2022-02-03 19:06:13 -0800

    made changes to some tests

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -12,35 +12,13 @@ public class MarkdownParseTest {
     }
     @Test
     public void getlink() throws IOException{
-        String[] args = {"test-file.md"};
-        Path fileName = Path.of(args[0]);
-	    String contents = Files.readString(fileName);
-        ArrayList<String> actual = MarkdownParse.getLinks(contents);
-        ArrayList<String> expected = new ArrayList();
-        expected.add("https://something.com");
-        expected.add("https://something.com");
-        assertEquals(actual,expected);
+        
+        assertEquals(1,3);
     }
     @Test
     public void test2() throws IOException{
-        String[] args = {"test-file2.md"};
-        Path fileName = Path.of(args[0]);
-	    String contents = Files.readString(fileName);
-        ArrayList<String> actual = MarkdownParse.getLinks(contents);
-        ArrayList<String> expected = new ArrayList();
-        expected.add("https://something.com");
-        expected.add("https://something.com");
-        assertEquals(actual,expected);
+        
+        assertEquals("cow","cow");
     }
-    @Test
-    public void test3() throws IOException{
-        String[] args = {"test-file3.md"};
-        Path fileName = Path.of(args[0]);
-	    String contents = Files.readString(fileName);
-        ArrayList<String> actual = MarkdownParse.getLinks(contents);
-        ArrayList<String> expected = new ArrayList();
-        expected.add("https://something.com");
-        expected.add("https://something.com");
-        assertEquals(actual,expected);
-    }
+   
 }
